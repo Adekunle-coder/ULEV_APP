6c0c291a01c2ebb1d14b0eb1c06212861fb81a5b
Adekunle Makanjuola <adekunle@Adekunles-MacBook-Pro.local>
2025-07-06 13:55:26 +0100

    Get_Image

--- get_image.py
+++ get_image.py
@@ -1,33 +1,33 @@
-import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import streamlit as st
 
 
+
+
 def get_image(vrm):
-    options = uc.ChromeOptions()
+    options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    
-    driver = uc.Chrome(options=options)
-
+    driver = webdriver.Chrome(options=options)
     url = f"https://totalcarcheck.co.uk/FreeCheck?regno={vrm}"
     img_url = ""
 
     try:
         driver.get(url)
-        # Use WebDriverWait to wait for the image to load
         image = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "vehicleImage"))
         )
-        img_url = image.get_attribute("src")
-        return img_url
-
-    except Exception as e:
-        st.info(f"An error was encountered while fetching the image: {e}")
+        if img_url:
+            img_url = image.get_attribute("src")
+            return img_url
+        else:
+            st.info("Image not found")
+    except:
+        st.info(f"An error was encountered while fetching the image")
     
     finally:
         driver.quit()
