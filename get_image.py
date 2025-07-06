0b14c05b6321126900fb780e8e842f3c5986a4c7
Adekunle Makanjuola <adekunle@Adekunles-MacBook-Pro.local>
2025-07-06 08:00:17 +0100

    Request update

--- get_image.py
+++ get_image.py
@@ -6,21 +6,21 @@ import streamlit as st
 def get_image(vrm: str) -> str | None:
 
     import requests
-    
-    referer = "https://totalcarcheck.co.uk/"
-    accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
-    accept_language = "en-GB,en;q=0.6"
-    cookie = "zero-chakra-ui-color-mode=light-zero; AMP_MKTG_8f1ede8e9c=JTdCJTIycmVmZXJyZXIlMjIlM0ElMjJodHRwcyUzQSUyRiUyRnd3dy5nb29nbGUuY29tJTJGJTIyJTJDJTIycmVmZXJyaW5nX2RvbWFpbiUyMiUzQSUyMnd3dy5nb29nbGUuY29tJTIyJTdE; AMP_8f1ede8e9c=JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI1MjgxOGYyNC05ZGQ3LTQ5OTAtYjcxMC01NTY0NzliMzAwZmYlMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzA4MzgxNTQ4ODQzJTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTcwODM4MjE1NTQ2MCUyQyUyMmxhc3RFdmVudElkJTIyJTNBNiU3RA=="
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+
+    # referer = "https://totalcarcheck.co.uk/"
+    # accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
+    # accept_language = "en-GB,en;q=0.6"
+    # cookie = "zero-chakra-ui-color-mode=light-zero; AMP_MKTG_8f1ede8e9c=JTdCJTIycmVmZXJyZXIlMjIlM0ElMjJodHRwcyUzQSUyRiUyRnd3dy5nb29nbGUuY29tJTJGJTIyJTJDJTIycmVmZXJyaW5nX2RvbWFpbiUyMiUzQSUyMnd3dy5nb29nbGUuY29tJTIyJTdE; AMP_8f1ede8e9c=JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjI1MjgxOGYyNC05ZGQ3LTQ5OTAtYjcxMC01NTY0NzliMzAwZmYlMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzA4MzgxNTQ4ODQzJTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTcwODM4MjE1NTQ2MCUyQyUyMmxhc3RFdmVudElkJTIyJTNBNiU3RA=="
+    # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
     
 
-    custom_headers = {
-        "User-Agent": user_agent,
-        "Accept": accept,
-        "Accept-Language": accept_language,
-        "Cookie": cookie,
-        "Referer": referer
-    }
+    # custom_headers = {
+    #     "User-Agent": user_agent,
+    #     "Accept": accept,
+    #     "Accept-Language": accept_language,
+    #     "Cookie": cookie,
+    #     "Referer": referer
+    # }
     
     url = f"https://totalcarcheck.co.uk/FreeCheck?regno={vrm}"
 
@@ -33,7 +33,12 @@ def get_image(vrm: str) -> str | None:
         #     )
         # }
 
-        response = requests.get(url, headers=custom_headers, timeout=10)
+        response = requests.get(
+            "https://api.browserless.io/scrape", params={
+            "url": url,
+            "token": "2Sd2biKOHYwY4n8d0b4c6dac775a910acfac1c97aa9ae50de"
+        })
+        
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, "html.parser")
