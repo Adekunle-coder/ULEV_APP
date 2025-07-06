68f1d9552995939763318b265be7bd1fa44bb9c5
Adekunle Makanjuola <adekunle@Adekunles-MacBook-Pro.local>
2025-07-06 02:50:01 +0100

    Added packages.txt

--- get_image.py
+++ get_image.py
@@ -1,29 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 import streamlit as st
-
-
-
 
 def get_image(vrm):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(options=options)
-    url = f"https://totalcarcheck.co.uk/FreeCheck?regno={vrm}"
-    img_url = ""
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # This is the Chromium path on Streamlit Cloud
+    options.binary_location = "/usr/bin/chromium-browser"
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
 
     try:
+        url = f"https://totalcarcheck.co.uk/FreeCheck?regno={vrm}"
         driver.get(url)
         image = driver.find_element(By.ID, "vehicleImage")
         if image:
-            img_url = image.get_attribute("src")
-            return img_url
-        else:
-            st.info("Image not found")
-    except:
-        st.info(f"An error was encountered while fetching the image")
-    
+            return image.get_attribute("src")
+    except Exception as e:
+        st.warning(f"Error while fetching vehicle image: {e}")
     finally:
         driver.quit()
