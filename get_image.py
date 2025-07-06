6af9844df113bc7ca9f9efdfa75e5b53570ed409
Adekunle Makanjuola <adekunle@Adekunles-MacBook-Pro.local>
2025-07-06 13:35:54 +0100

    Updated get_image to use uc

--- get_image.py
+++ get_image.py
@@ -1,27 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import streamlit as st
 
 
-
-
 def get_image(vrm):
-    options = Options()
+    options = uc.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(options=options)
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    
+    driver = uc.Chrome(options=options)
+
     url = f"https://totalcarcheck.co.uk/FreeCheck?regno={vrm}"
     img_url = ""
 
     try:
         driver.get(url)
-        image = driver.find_element(By.ID, "vehicleImage")
-        if image:
-            img_url = image.get_attribute("src")
-            return img_url
-        else:
-            st.info("Image not found")
+        # Use WebDriverWait to wait for the image to load
+        image = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "vehicleImage"))
+        )
+        img_url = image.get_attribute("src")
+        return img_url
+
     except Exception as e:
         st.info(f"An error was encountered while fetching the image: {e}")
     
